640ed6591ab5d811e889f491c68ff7bb9f817ee6
hrga\Luka Hrgović <luka.hrgovic@fer.hr>
2017-01-20 22:00:42 +0100

    ingredient repositories modified

--- PizzaRepository/IIngredientRepository.cs
+++ PizzaRepository/IIngredientRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PizzaSharp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace PizzaSharp
 {
-    public class IIngredientRepository
+    public interface IIngredientRepository
     {
+        Ingredient Get(Ingredient ingredient);
+
+        List<Ingredient> GetAll();
+
+        void Add(Ingredient ingredient);
+
+        void Update(Ingredient ingredient);
     }
 }
--- PizzaRepository/IngredientSqlRepository.cs
+++ PizzaRepository/IngredientSqlRepository.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PizzaSharp.Models;
 
 namespace PizzaSharp
 {
     public class IngredientSqlRepository : IIngredientRepository
     {
+        private readonly PizzaDbContext _context;
+        public IngredientSqlRepository(PizzaDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Ingredient ingredient)
+        {
+            Ingredient oldIngredient = Get(ingredient);
+
+            if (oldIngredient == null)
+                _context.Ingredients.Add(ingredient);
+            else
+                throw new ArgumentException("The given ingredient already exists!");
+
+            _context.SaveChanges();
+        }
+
+        public Ingredient Get(Ingredient ingredient)
+        {
+            return _context
+                .Ingredients
+                .FirstOrDefault(s => s.Name.Equals(ingredient.Name));
+        }
+
+        public List<Ingredient> GetAll()
+        {
+            return _context
+                .Ingredients
+                .ToList();
+        }
+
+        public void Update(Ingredient ingredient)
+        {
+            Ingredient oldIngredient = Get(ingredient);
+
+            if (oldIngredient != null)
+                oldIngredient.UpdateValues(ingredient);
+            else
+                throw new ArgumentException("The given ingredient doesn't exist!");
+
+            _context.SaveChanges();
+        }
     }
 }
