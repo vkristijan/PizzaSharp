378e3a0c1196892c8569edbbad64032d5eb40259
Kristijan <v.kristijan@gmail.com>
2017-01-21 23:29:18 +0100

    Changed home controler index page Added bootstrap template page to the home page, removed unused and duplicate libraries

--- ActivelyServedPizza/wwwroot/_references.js
+++ ActivelyServedPizza/wwwroot/_references.js
@@ -1,6 +1,4 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="../scripts/bootstrap.js" />
-/// <reference path="../scripts/jquery.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
