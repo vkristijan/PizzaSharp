f0d58086d4c918401d6987e9a669f57da72cbd91
hrga\Luka Hrgović <luka.hrgovic@fer.hr>
2017-01-22 22:45:17 +0100

    OrderViewModel added

---
+++ ActivelyServedPizza/Models/OrderViewModels/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PizzaSharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActivelyServedPizza.Models.OrderViewModels
+{
+    public class OrderViewModel
+    {
+        public Pizza Pizza {get; set; }
+        public int Size { get; set; }
+        public int Amount { get; set; }
+    }
+}
